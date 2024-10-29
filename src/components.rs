0cd3c1b8d5ef99be6b11239ef1462fe51365362e
Jiashu <790548118@qq.com>
2024-10-29 11:31:25 +0800

    implement combat

--- src/components.rs
+++ src/components.rs
@@ -29,3 +29,9 @@ pub struct Health {
 
 #[derive(Clone, PartialEq)]
 pub struct Name(pub String);
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub struct WantToAttack {
+    pub attacker: Entity,
+    pub victim: Entity,
+}
